f0d14f66a88cd60a3fa5f85b1027dc69bfa9f404
Justin Hall <hall3jl@mail.uc.edu>
2020-11-22 21:09:46 -0500

    first commit

--- tweet.py
+++ tweet.py
@@ -8,6 +8,7 @@ req = requests.get("https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,re
 joke_json = req.json()
 joke_setup = (joke_json['setup'])
 
+# Create object to hold API keys
 twitter_auth_keys = {
     "consumer_key"        : os.environ.get('consumer_key'),
     "consumer_secret"     : os.environ.get('consumer_secret'),
@@ -15,14 +16,20 @@ twitter_auth_keys = {
     "access_token_secret" : os.environ.get('access_token_secret')
 }
 
+# Create object to handle OAuth 
 auth = tweepy.OAuthHandler(
     twitter_auth_keys['consumer_key'],
     twitter_auth_keys['consumer_secret']
 )
+
+# Add access tokens to object
 auth.set_access_token(
     twitter_auth_keys['access_token'],
     twitter_auth_keys['access_token_secret']
 )
+
+# Auth with Twitter
 api = tweepy.API(auth)
 
+# Tweet the "joke"
 status = api.update_status(status=joke_setup)
